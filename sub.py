4b892d3e67cd011fefe76bea4e0498fe71d5b7b6
ty0207 <ty0207@sjtu.edu.cn>
2020-10-09 15:39:17 +0800

    else if

--- sub.py
+++ sub.py
@@ -18,7 +18,7 @@ for item in p.listen():
         if len(item['data']) > 4 :
             with open('received.jpg', 'wb') as f:
                 f.write(item['data'])
-        else if item['data'].decode()=='over':
+        elif item['data'].decode()=='over':
             print(item['channel'].decode(),'停止发布')
             break
 p.unsubscribe('spub')
