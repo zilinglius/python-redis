a03d1983f0498eb7b215942f41c5db92e20c6818
ty0207 <ty0207@sjtu.edu.cn>
2020-10-09 13:01:04 +0800

    array

--- pub.py
+++ pub.py
@@ -3,9 +3,11 @@ import redis
 pool=redis.ConnectionPool(host='127.0.0.1',
                           port=6379,db=0)
 r=redis.StrictRedis(connection_pool=pool)
+data = [1,2,3]
 while True:
     msg=input("publish: >>")
-    r.publish('spub',msg)
     if msg=="over":
         print("停止发布")
+        r.publish('spub',)
         break
+    r.publish('spub', data)
