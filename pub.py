3a3aa4a39888b5e77ba38e67d78f976545e26c9e
ty0207 <ty0207@sjtu.edu.cn>
2020-10-09 15:51:38 +0800

    time

--- pub.py
+++ pub.py
@@ -1,15 +1,20 @@
 import io
 from PIL import Image
 import redis
+import time
 
 pool=redis.ConnectionPool(host='127.0.0.1',
                           port=6379,db=0)
 r=redis.StrictRedis(connection_pool=pool)
 
 img = Image.open('test.jpg', mode='r')
+
+start_time = time.time()
 imgByteArr = io.BytesIO()
 img.save(imgByteArr, format='JPEG')
 imgByteArr = imgByteArr.getvalue()
+end_time = time.time()
+print('time cost',time_end-time_start,'s')
 
 r.publish('spub', imgByteArr)
 
