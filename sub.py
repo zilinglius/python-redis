fa2829f3ea475e84e1331888a81b8f7537074702
ty0207 <ty0207@sjtu.edu.cn>
2020-10-09 15:36:20 +0800

    save file

--- sub.py
+++ sub.py
@@ -1,15 +1,24 @@
+import os
+import io
+from PIL import Image
 import redis
+
 pool=redis.ConnectionPool(host='127.0.0.1',
                           port=6379,db=0)
+
 r=redis.StrictRedis(connection_pool=pool)
 p=r.pubsub()
 p.subscribe("spub","cctv1")
+
 for item in p.listen():
     print("Listen on channel : %s "%item['channel'].decode())
     if item['type']=='message':
         data=item['data'].decode()
         print("From %s get message : %s"%(item['channel'].decode(),item['data'].decode()))
-        if item['data'].decode()=='over':
+        if len(item['data']) > 4 :
+            with open('received.jpg', 'wb') as f:
+                f.write(item['data'])
+        else if item['data'].decode()=='over':
             print(item['channel'].decode(),'停止发布')
             break
 p.unsubscribe('spub')
