8cde30b467509d3cbcbc7bdbc69cee1e4ac021e6
ty0207 <ty0207@sjtu.edu.cn>
2020-10-09 14:54:07 +0800

    data

--- pub.py
+++ pub.py
@@ -3,6 +3,7 @@ import redis
 pool=redis.ConnectionPool(host='127.0.0.1',
                           port=6379,db=0)
 r=redis.StrictRedis(connection_pool=pool)
+
 data = [1,2,3]
 while True:
     msg=input("publish: >>")
