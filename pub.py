d798224897581e4c0deecc89bf94ccea36254fe2
ty0207 <ty0207@sjtu.edu.cn>
2020-10-09 15:07:58 +0800

    jpg

--- pub.py
+++ pub.py
@@ -1,14 +1,22 @@
-
+import io
 import redis
 pool=redis.ConnectionPool(host='127.0.0.1',
                           port=6379,db=0)
 r=redis.StrictRedis(connection_pool=pool)
 
-data = [1,2,3]
+img = Image.open('test.jpg', mode='r')
+imgByteArr = io.BytesIO()
+img.save(imgByteArr, format='JPEG')
+imgByteArr = imgByteArr.getvalue()
+
+data = bytes('http://www.baidu.com', )
+
+r.publish('spub', imgByteArr)
+
 while True:
     msg=input("publish: >>")
     if msg=="over":
         print("停止发布")
         r.publish('spub',)
         break
-    r.publish('spub', data)
+    r.publish('spub', imgByteArr)
